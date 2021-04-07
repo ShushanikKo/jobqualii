982175674d1abb175b35cc29f8ffed948c06cac0
shushanik.kostumyan <shushanik.kostumyan@gmail.com>
2021-04-07 16:56:38 +0400

    firstcommit

---
+++ src/main/java/rcrd/demo_version/App.java
@@ -0,0 +1,13 @@
+package rcrd.demo_version;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
